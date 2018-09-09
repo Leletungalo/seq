cf5414606bcef165c295b4b5feb1a513daf086bc
leletu ngalo <leletungalo@gmail.com>
2018-09-09 14:42:59 +0200

    after timing methods

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,11 +2,36 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-        readData readData = new readData("sample_input.txt");
+    static long startTime = 0;
 
-        readData.CalculateAvrg();
-        readData.countMean();
+    private static void tick(){
+        startTime = System.currentTimeMillis();
+    }
+    private static float tock(){
+        return (System.currentTimeMillis() - startTime) / 1000.0f;
+    }
+    /*static final ForkJoinPool fjPool = new ForkJoinPool();
+    static int sum(int[] arr){
+        return fjPool.invoke(new SumArray(arr,0,arr.length));
+    }*/
+
+    public static void main(String[] args) {
+        if (args.length > 0){
+        tick();
+            readData readData = new readData("sample_input.txt"/*args[0]*/);
+            float nun = tock();
+            System.out.println("Time is : " + nun);
+            tick();
+            readData.CalculateAvrg();
+            readData.countMean();
+            float vvv = tock();
+        System.out.println("Time is : " + vvv);
+        }else
+            System.out.println("nothing");
+
+        //*readData readData = new readData("sample_input.txt");
+
+
 
     }
 }
